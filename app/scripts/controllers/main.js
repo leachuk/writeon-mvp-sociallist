38816f7745d9b88437514d04ccba73624e70961e
leachuk <stew.leach@gmail.com>
2015-05-10 23:01:49 +1000

    working client authentication against remote API

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,7 +1,30 @@
 'use strict';
 
 angular.module('sociallistMvp1App')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $http) {
+  	$scope.user = {};
+
+  	$scope.userSignIn = function(){
+		var data = {};
+		data.username = $scope.user.email;
+		data.password = $scope.user.password;
+		console.log(data);
+
+		//abstract to an auth API service
+		$http.post('http://localhost:9000/api/users/signin', data).
+		success(function(outdata, status, headers, config) {
+			console.log(outdata);
+			window.localStorage.setItem("writeon.authtoken", outdata.token);
+			window.localStorage.setItem("writeon.username", data.username);
+			//now redirect to users home page, where token is checked for
+			window.location = "/home"; //will prob need to change this to come from header referer
+		}).
+		error(function(data, status, headers, config) {
+			console.log("Invalid login attempt: " + data);
+			$scope.signinForm.$setValidity("unauthorised", false);
+		});
+  	};
+
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
